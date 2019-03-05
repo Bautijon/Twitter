464f26d259479ebe64c9fb173da85b78fd97bbe9
Jonathan <jonathanbautista@jonathans-macbook-pro.local>
2019-03-05 14:37:59 -0600

    Commited

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -109,7 +109,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImage.image = UIImage(data: imageData)
         }
         
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
